12ddc8b9ac0afd38894ec22422749872abfb0516
Leeann Drees <leeann.drees@detroitlabs.com>
2019-02-14 09:56:38 -0500

    initial commit

---
+++ dressForTheWeather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  dressForTheWeather
+//
+//  Created by Leeann Drees on 2/14/19.
+//  Copyright © 2019 DetroitLabs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
