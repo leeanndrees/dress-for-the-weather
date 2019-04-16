c48e3ae6f2ad449e8db436fa3890adb84bb70a82
Liz Levosinski <levosins@gmail.com>
2019-04-16 13:22:05 -0400

    deleted file

--- dressForTheWeather/LocationManager.swift
+++
@@ -1,9 +0,0 @@
-//
-//  LocationManager.swift
-//  dressForTheWeather
-//
-//  Created by Elizabeth Levosinski on 4/11/19.
-//  Copyright © 2019 DetroitLabs. All rights reserved.
-//
-
-import Foundation
