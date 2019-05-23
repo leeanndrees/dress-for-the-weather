b69acabe3f7c29d0271d58478459deb7b9cfaa44
Leeann Drees <leeann.drees@detroitlabs.com>
2019-05-23 10:08:02 -0400

    fix method that got broken in merge

--- dressForTheWeather/View Models/RecommendationViewModel.swift
+++ dressForTheWeather/View Models/RecommendationViewModel.swift
@@ -18,9 +18,9 @@ protocol RecommendationViewDelegate: AnyObject {
 }
 
 final class RecommendationViewModel {
-
+    
     // MARK: - Properties
-
+    
     weak var delegate: RecommendationViewDelegate?
     private var lowTemp: Double = 0
     private var highTemp: Double = 0
@@ -38,59 +38,62 @@ final class RecommendationViewModel {
         didSet {
             let latitude = location!.coordinate.latitude
             let longitude = location!.coordinate.longitude
-
+            
             getTemperature(latitude: latitude, longitude: longitude)
         }
     }
     private let userLocationManager = UserLocationManager()
-
+    
     // MARK: - Initializer
-
+    
     init(delegate: RecommendationViewDelegate) {
         self.delegate = delegate
         setup()
     }
-
+    
     // MARK: - Methods
-
+    
     private func setup() {
         userLocationManager.delegate = self
         getLocation()
     }
-
+    
     private func getLocation() {
         userLocationManager.getLocation()
     }
-
+    
     private func getTemperature(latitude: Double, longitude: Double) {
-        WeatherNetworking.getWeatherFor(latitude: latitude, longitude: longitude) { weatherData in
+        WeatherNetworking.getWeatherFor(latitude: latitude, longitude: longitude, success: { weatherData in
             self.lowTemp = weatherData.daily.data[0].temperatureLow
             self.highTemp = weatherData.daily.data[0].temperatureHigh
             self.setRecommendations(for: self.lowTemp, highTemp: self.highTemp)
             self.temperature = weatherData.currently.temperature
+        }, failure: { error in
+            self.delegate?.didFailToGetWeather(error.localizedDescription)
         }
+        )
     }
-
+    
     private func generateRecommendation(for lowTemp: Double, highTemp: Double, from items: [ClothingItem]) -> [ClothingItem] {
         return items.filter { $0.tempRange.contains(lowTemp) || $0.tempRange.contains(highTemp) }
     }
-
+    
     private func setRecommendations(for lowTemp: Double, highTemp: Double) {
         let recommendedItems = generateRecommendation(for: lowTemp, highTemp: highTemp, from: allClothingItems)
         let outfit = Outfit(components: recommendedItems)
         recommendations = outfit.recommendations
     }
-
+    
 }
 
 extension RecommendationViewModel: UserLocationManagerDelegate {
     func didFail(errorDescription: String) {
         delegate?.didFailToGetWeather(errorDescription)
     }
-
-
+    
+    
     func didGetLocation() {
         location = userLocationManager.location
     }
-
+    
 }
