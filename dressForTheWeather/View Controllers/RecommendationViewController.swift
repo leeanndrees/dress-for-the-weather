59507acff4830903853017e9c1ef888dba2d7b70
Leeann Drees <leeann.drees@detroitlabs.com>
2019-05-24 11:21:47 -0400

    gradient background

--- dressForTheWeather/View Controllers/RecommendationViewController.swift
+++ dressForTheWeather/View Controllers/RecommendationViewController.swift
@@ -40,7 +40,17 @@ final class RecommendationViewController: UIViewController {
     }
     
     private func setBackgroundColor() {
-        view.backgroundColor = UIColor(named: "mild")
+        
+        guard let mildGreenColor = UIColor(named: "mildGreen"),
+            let warmOrangeColor = UIColor(named: "warmOrange") else { return }
+        
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [mildGreenColor.cgColor, warmOrangeColor.cgColor]
+        gradientLayer.startPoint = CGPoint(x: 0, y: 0)
+        gradientLayer.endPoint = CGPoint(x: 1, y: 1.5)
+        gradientLayer.frame = view.bounds
+        
+        view.layer.insertSublayer(gradientLayer, at: 0)
     }
 
 }
