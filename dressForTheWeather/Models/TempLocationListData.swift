4618d54cc465736f7f15d601ff5f85af77b6a150
Leeann Drees <leeann.drees@detroitlabs.com>
2019-07-11 11:11:00 -0400

    create and display temporary location list

---
+++ dressForTheWeather/Models/TempLocationListData.swift
@@ -0,0 +1,15 @@
+//
+//  TempLocationListData.swift
+//  dressForTheWeather
+//
+//  Created by Leeann Drees on 7/11/19.
+//  Copyright © 2019 DetroitLabs. All rights reserved.
+//
+
+import Foundation
+
+let storedLocationNames: [(city: String, state: String)] = [
+    ("Detroit", "MI"),
+    ("Tampa", "FL"),
+    ("San Francisco", "CA")
+]
